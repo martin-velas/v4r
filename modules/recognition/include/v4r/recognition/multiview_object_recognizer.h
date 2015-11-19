3b98f3744c85cd8179fbcdf8abddd62c5c670b66
Martin Velas <ivelas@fit.vutbr.cz>
2015-11-19 13:14:14 +0100

    Fix: change detection history for reconstruction

--- modules/recognition/include/v4r/recognition/multiview_object_recognizer.h
+++ modules/recognition/include/v4r/recognition/multiview_object_recognizer.h
@@ -117,7 +117,7 @@ protected:
     bool computeAbsolutePose(CamConnect & e, bool is_first_edge = false);
     
     std::map < size_t, typename pcl::PointCloud<PointT>::Ptr > removed_points_history_;
-    std::map < size_t, typename pcl::PointCloud<PointT>::Ptr > added_points_history_;
+    typename pcl::PointCloud<PointT>::Ptr added_points_;
     pcl::PointCloud<PointT> changes_visualization;
 
     /** \brief removes vertices from graph if max_vertices_in_graph has been reached */
